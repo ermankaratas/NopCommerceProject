5a11f8fcca5072f815e8033ee6c0f2783dba1553
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2024-03-31 00:22:33 +0100

    TabMenu Test Case is initialized.

---
+++ src/TestCases/US504_TabMenu.java
@@ -0,0 +1,66 @@
+package TestCases;
+
+import Utility.BaseDriver;
+import Utility.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class US504_TabMenu extends BaseDriver {
+    @Test
+    public void TC504(){
+        Elements elements = new Elements();
+
+        Actions actions = new Actions(driver);
+
+        List<String> menuElements = Arrays.asList("Computers","Electronics","Apparel","Digital downloads",
+                "Books","Jewelry","Gift Cards");
+
+        for (int i = 0; i < menuElements.size(); i++)
+            Assert.assertTrue(elements.tabMenus.get(i).getText().equals(menuElements.get(i)));
+
+        actions.moveToElement(elements.computers).build().perform();
+        actions.moveToElement(elements.desktops).click().build().perform();
+        wait.until(ExpectedConditions.urlContains("desktops"));
+        Assert.assertTrue(elements.title.getText().equals("Desktops"));
+        actions.moveToElement(elements.computers).build().perform();
+        actions.moveToElement(elements.notebooks).click().build().perform();
+        Assert.assertTrue(elements.title.getText().equals("Notebooks"));
+        actions.moveToElement(elements.computers).build().perform();
+        actions.moveToElement(elements.software).click().build().perform();
+        Assert.assertTrue(elements.title.getText().equals("Software"));
+        actions.moveToElement(elements.electronics).build().perform();
+        actions.moveToElement(elements.camera).click().build().perform();
+        Assert.assertTrue(elements.title.getText().equals("Camera & photo"));
+        actions.moveToElement(elements.electronics).build().perform();
+        actions.moveToElement(elements.cellPhone).click().build().perform();
+        Assert.assertTrue(elements.title.getText().equals("Cell phones"));
+        actions.moveToElement(elements.electronics).build().perform();
+        actions.moveToElement(elements.others).click().build().perform();
+        Assert.assertTrue(elements.title.getText().equals("Others"));
+        actions.moveToElement(elements.apparel).build().perform();
+        actions.moveToElement(elements.shoes).click().build().perform();
+        Assert.assertTrue(elements.title.getText().equals("Shoes"));
+        actions.moveToElement(elements.apparel).build().perform();
+        actions.moveToElement(elements.clothing).click().build().perform();
+        Assert.assertTrue(elements.title.getText().equals("Clothing"));
+        actions.moveToElement(elements.apparel).build().perform();
+        actions.moveToElement(elements.accessories).click().build().perform();
+        Assert.assertTrue(elements.title.getText().equals("Accessories"));
+        actions.moveToElement(elements.downloads).click().build().perform();
+        Assert.assertTrue(elements.title.getText().equals("Digital downloads"));
+        actions.moveToElement(elements.books).click().build().perform();
+        Assert.assertTrue(elements.title.getText().equals("Books"));
+        actions.moveToElement(elements.jewelry).click().build().perform();
+        Assert.assertTrue(elements.title.getText().equals("Jewelry"));
+        actions.moveToElement(elements.giftCards).click().build().perform();
+        Assert.assertTrue(elements.title.getText().equals("Gift Cards"));
+
+    }
+}
--- src/Utility/Elements.java
+++ src/Utility/Elements.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class Elements {
     public Elements(){
         PageFactory.initElements(BaseDriver.driver,this);
@@ -42,6 +44,42 @@ public class Elements {
     public WebElement loginSubmitButton;
     @FindBy(xpath = "//div[@class='message-error validation-summary-errors']")
     public WebElement loginNotConfirmText;
+    @FindBy(xpath = "//ul[@class='top-menu notmobile']/li")
+    public List<WebElement> tabMenus;
+    @FindBy(linkText = "Computers")
+    public WebElement computers;
+    @FindBy(xpath = "//ul[@class='top-menu notmobile']/li/ul/li/a")
+    public WebElement desktops;
+    @FindBy(xpath = "//ul[@class='top-menu notmobile']/li/ul/li[2]/a")
+    public WebElement notebooks;
+    @FindBy(xpath = "//ul[@class='top-menu notmobile']/li/ul/li[3]/a")
+    public WebElement software;
+    @FindBy(linkText = "Electronics")
+    public WebElement electronics;
+    @FindBy(xpath = "//ul[@class='top-menu notmobile']/li[2]/ul/li/a")
+    public WebElement camera;
+    @FindBy(xpath = "//ul[@class='top-menu notmobile']/li[2]/ul/li[2]/a")
+    public WebElement cellPhone;
+    @FindBy(xpath = "//ul[@class='top-menu notmobile']/li[2]/ul/li[3]/a")
+    public WebElement others;
+    @FindBy(linkText = "Apparel")
+    public WebElement apparel;
+    @FindBy(xpath = "//ul[@class='top-menu notmobile']/li[3]/ul/li/a")
+    public WebElement shoes;
+    @FindBy(xpath = "//ul[@class='top-menu notmobile']/li[3]/ul/li[2]/a")
+    public WebElement clothing;
+    @FindBy(xpath = "//ul[@class='top-menu notmobile']/li[3]/ul/li[3]/a")
+    public WebElement accessories;
+    @FindBy(linkText = "Digital downloads")
+    public WebElement downloads;
+    @FindBy(linkText = "Books")
+    public WebElement books;
+    @FindBy(linkText = "Jewelry")
+    public WebElement jewelry;
+    @FindBy(linkText = "Gift Cards")
+    public WebElement giftCards;
+    @FindBy(xpath = "//div[@class='page-title']")
+    public WebElement title;
 }
 
 
