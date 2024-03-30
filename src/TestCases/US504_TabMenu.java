3783a308d3097a824c23ba861e3ad9a90b13258d
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2024-03-31 00:44:52 +0100

    Action is changed.

--- src/TestCases/US504_TabMenu.java
+++ src/TestCases/US504_TabMenu.java
@@ -2,8 +2,8 @@ package TestCases;
 
 import Utility.BaseDriver;
 import Utility.Elements;
+import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -23,42 +23,49 @@ public class US504_TabMenu extends BaseDriver {
         for (int i = 0; i < menuElements.size(); i++)
             Assert.assertTrue(elements.tabMenus.get(i).getText().equals(menuElements.get(i)));
 
-        actions.moveToElement(elements.computers).build().perform();
+        Action action = actions.moveToElement(elements.computers).build();
+        action.perform();
         actions.moveToElement(elements.desktops).click().build().perform();
-        wait.until(ExpectedConditions.urlContains("desktops"));
         Assert.assertTrue(elements.title.getText().equals("Desktops"));
-        actions.moveToElement(elements.computers).build().perform();
+        action.perform();
         actions.moveToElement(elements.notebooks).click().build().perform();
         Assert.assertTrue(elements.title.getText().equals("Notebooks"));
-        actions.moveToElement(elements.computers).build().perform();
+        action.perform();
         actions.moveToElement(elements.software).click().build().perform();
         Assert.assertTrue(elements.title.getText().equals("Software"));
-        actions.moveToElement(elements.electronics).build().perform();
+
+        action = actions.moveToElement(elements.electronics).build();
+        action.perform();
         actions.moveToElement(elements.camera).click().build().perform();
         Assert.assertTrue(elements.title.getText().equals("Camera & photo"));
-        actions.moveToElement(elements.electronics).build().perform();
+        action.perform();
         actions.moveToElement(elements.cellPhone).click().build().perform();
         Assert.assertTrue(elements.title.getText().equals("Cell phones"));
-        actions.moveToElement(elements.electronics).build().perform();
+        action.perform();
         actions.moveToElement(elements.others).click().build().perform();
         Assert.assertTrue(elements.title.getText().equals("Others"));
-        actions.moveToElement(elements.apparel).build().perform();
+
+        action = actions.moveToElement(elements.apparel).build();
+        action.perform();
         actions.moveToElement(elements.shoes).click().build().perform();
         Assert.assertTrue(elements.title.getText().equals("Shoes"));
-        actions.moveToElement(elements.apparel).build().perform();
+        action.perform();
         actions.moveToElement(elements.clothing).click().build().perform();
         Assert.assertTrue(elements.title.getText().equals("Clothing"));
-        actions.moveToElement(elements.apparel).build().perform();
+        action.perform();
         actions.moveToElement(elements.accessories).click().build().perform();
         Assert.assertTrue(elements.title.getText().equals("Accessories"));
+
         actions.moveToElement(elements.downloads).click().build().perform();
         Assert.assertTrue(elements.title.getText().equals("Digital downloads"));
+
         actions.moveToElement(elements.books).click().build().perform();
         Assert.assertTrue(elements.title.getText().equals("Books"));
+
         actions.moveToElement(elements.jewelry).click().build().perform();
         Assert.assertTrue(elements.title.getText().equals("Jewelry"));
+
         actions.moveToElement(elements.giftCards).click().build().perform();
         Assert.assertTrue(elements.title.getText().equals("Gift Cards"));
-
     }
 }
