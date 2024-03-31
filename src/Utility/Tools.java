7ddb5e98ef7c20c8c4dd4f65f6c4fa3d6c7fa60e
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2024-04-01 01:17:35 +0200

    randomGenerator method is created

--- src/Utility/Tools.java
+++ src/Utility/Tools.java
@@ -1,12 +1,16 @@
 package Utility;
 
 public class Tools {
-    public static void wait(int sec){
+    public static void wait(int sec) {
 
         try {
-            Thread.sleep(sec*1000);
+            Thread.sleep(sec * 1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static int randomGenerator(int number) {
+        return (int) (Math.random() * number);
     }
 }
