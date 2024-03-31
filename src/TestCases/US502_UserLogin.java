95f004cf86ec412a295024155b0c63505e2bdb7b
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2024-04-01 01:45:58 +0200

    one import is deleted

--- src/TestCases/US502_UserLogin.java
+++ src/TestCases/US502_UserLogin.java
@@ -4,7 +4,6 @@ import Utility.Elements;
 import Utility.Tools;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
